d96a812eebd27202041c29ec5a6a365f449af2be
RamReddy97 <babblu111reddy@gmail.com>
2026-02-03 12:34:11 +0530

    Update concatenation-of-array.java

--- 2058-concatenation-of-array/concatenation-of-array.java
+++ 2058-concatenation-of-array/concatenation-of-array.java
@@ -11,4 +11,18 @@ class Solution {
         return ans;
     }
 
+    /* older code 
+
+     int l = nums.length;
+        int[] ans = new int[2 * l ];
+        for (int i = 0; i < ans.length; i++) {
+
+            if (i >= l)
+                ans[i] = nums[i - l];
+            else
+                ans[i] = nums[i];
+        }
+        return ans;
+        
+    */
 }
