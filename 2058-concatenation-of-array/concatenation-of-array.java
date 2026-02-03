257333a5a64c3ed9cc1195ae2939cb5534791276
RamReddy97 <babblu111reddy@gmail.com>
2026-02-03 13:49:18 +0530

    added comments on removing if else statement

--- 2058-concatenation-of-array/concatenation-of-array.java
+++ 2058-concatenation-of-array/concatenation-of-array.java
@@ -11,13 +11,13 @@ class Solution {
         return ans;
     }
 
-    /* older code 
+    /* older code ie my own
 
      int l = nums.length;
         int[] ans = new int[2 * l ];
         for (int i = 0; i < ans.length; i++) {
 
-            if (i >= l)
+            if (i >= l)    // idea is to remove this if else conditions as it takes time to check these
                 ans[i] = nums[i - l];
             else
                 ans[i] = nums[i];
