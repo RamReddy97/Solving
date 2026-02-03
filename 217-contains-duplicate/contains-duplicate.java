b100d7c130a0d48d4ec609b9df7134a76232a339
RamReddy97 <babblu111reddy@gmail.com>
2026-02-03 17:44:49 +0530

    Time: 16 ms (46.39%) | Memory: 98.2 MB (6.99%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -5,6 +5,11 @@ class Solution {
 
         for (int i = 0; i < nums.length; i++) {
 
+            /*In a HashMap:
+            
+            containsKey() is O(1) (instant). It uses a mathematical formula to go straight to the location.
+            containsValue() is O(n). loop has to go through every single value in the map to find a match.
+            Because we are calling containsValue() (O(n)) inside a loop (O(n)), your total time complexity becomes O(n²).  */
             if (h.containsKey(nums[i]))
                 return true;
             else
